16d5e7baa26b889e04516179cd2777cdb3a357f7
mauricio.salazar-duque <mauricio.salazar-duque@enexis.nl>
2025-03-06 16:54:38 +0100

    Added an example

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import pandas as pd
 from spheral import Sphere
+from pathlib import Path
 
 
 
+#%%
+rlp_file = Path('data/raw/rlps_enschede_2023.csv')
+frame = pd.read_csv(rlp_file)
+
+#%%
+frame_pivoted = frame.pivot_table(index=["BOXID", "MONTH"], columns="QUARTER", values="POWER").reset_index()
+frame_pivoted.columns.name = None
+frame_filtered = frame_pivoted[frame_pivoted["MONTH"]==1].copy()
+frame_filtered.drop(columns=["MONTH"], inplace=True)
+frame_filtered.set_index("BOXID", inplace=True)
+
+#%%
+sphere = Sphere()
+sphere.fit(frame_filtered)
