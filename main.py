c939fd46db61668034d5665ec8954c6ef4e9d99f
mauricio.salazar-duque <mauricio.salazar-duque@enexis.nl>
2025-03-06 17:00:24 +0100

    Added an example

--- main.py
+++ main.py
@@ -5,16 +5,10 @@ from pathlib import Path
 
 
 #%%
-rlp_file = Path('data/raw/rlps_enschede_2023.csv')
-frame = pd.read_csv(rlp_file)
+rlp_file = Path('data/processed/rlps_filtered.csv')
+frame = pd.read_csv(rlp_file, index_col=0)
 
-#%%
-frame_pivoted = frame.pivot_table(index=["BOXID", "MONTH"], columns="QUARTER", values="POWER").reset_index()
-frame_pivoted.columns.name = None
-frame_filtered = frame_pivoted[frame_pivoted["MONTH"]==1].copy()
-frame_filtered.drop(columns=["MONTH"], inplace=True)
-frame_filtered.set_index("BOXID", inplace=True)
 
 #%%
 sphere = Sphere()
-sphere.fit(frame_filtered)
+sphere.fit(frame)
